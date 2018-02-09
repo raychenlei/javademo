626ebb365e31bd6b544280838c0057309a2a9212
chenlei10 <chenlei10@le.com>
2018-02-09 18:33:21 +0800

    加了一点测试类

---
+++ src/main/java/collection/ListTest.java
@@ -0,0 +1,35 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @Author chenlei10
+ * @Date 2018/1/31 11:33
+ */
+public class ListTest {
+    public void delete(){
+        List<Integer> list1 = new ArrayList<Integer>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+
+
+
+        for (Integer element : list1) {
+            list1.remove(0);
+        }
+
+        for (int i = 0; i < list1.size(); i++) {
+            //
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ src/main/java/demo/MyTest.java
@@ -0,0 +1,19 @@
+package demo;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Author chenlei10
+ * @Date 2018/2/9 17:37
+ */
+public class MyTest {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4,5);
+
+        List<Integer> collect = list.stream().filter(t -> t > 5).collect(Collectors.toList());
+        System.out.println(collect.size());
+
+    }
+}
