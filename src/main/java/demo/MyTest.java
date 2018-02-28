85a24c03ad80d4441fa85e74c74a8cac41809576
chenlei10 <chenlei10@le.com>
2018-02-28 18:42:56 +0800

    快速排序

--- src/main/java/demo/MyTest.java
+++ src/main/java/demo/MyTest.java
@@ -1,8 +1,6 @@
 package demo;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @Author chenlei10
@@ -10,10 +8,38 @@ import java.util.stream.Collectors;
  */
 public class MyTest {
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1,2,3,4,5);
+//        List<Integer> list = Arrays.asList(1,2,3,4,5);
+//
+//        List<Integer> collect = list.stream().filter(t -> t > 5).collect(Collectors.toList());
+//        System.out.println(collect.size());
 
-        List<Integer> collect = list.stream().filter(t -> t > 5).collect(Collectors.toList());
-        System.out.println(collect.size());
+        int[] array = {6,1 ,2 ,7,  9,  3,  4,  5, 10,  8};
+        quickSort(array,0,9);
+        System.out.println(Arrays.toString(array));
 
+    }
+
+    public static void quickSort(int[] array, int i, int j){
+
+        int index = i;
+        int p = array[i];
+
+        while (i<j){
+            while (array[j] > p) {j--;}
+
+            while (array[i] <= p) {i ++;}
+
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+            System.out.println(Arrays.toString(array));
+        }
+
+        int temp = array[index];
+        array[index] = array[i];
+        array[i] = temp;
+
+        quickSort(array, 0,i-1);
+        quickSort(array, j+1, array.length-1);
     }
 }
